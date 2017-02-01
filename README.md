608c1cf1fad1c5279bfc8d05400ffbb51cb01dc2
KenMarquez <darius_dam@hotmail.com>
2017-01-31 21:24:17 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# DevFB13Android
---
+++ app/src/main/java/com/ken/devfb13/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ken.devfb13;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
