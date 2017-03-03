ed77983c0201aa81ad1caa96f860f61320564e4c
KenMarquez <darius_dam@hotmail.com>
2017-03-02 21:50:18 -0600

    Add navigation view

--- navigationview/src/main/java/com/ken/navigationview/MainActivity.java
+++ navigationview/src/main/java/com/ken/navigationview/MainActivity.java
@@ -1,13 +1,84 @@
 package com.ken.navigationview;
 
+import android.support.annotation.BoolRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.ken.navigationview.fragments.PrimerFragment;
+import com.ken.navigationview.fragments.SegundoFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.drawer)
+    DrawerLayout drawerLayout;
+
+    @BindView(R.id.navigation_view)
+    NavigationView navigationView;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        configureNavigationView();
+    }
+
+    private void configureNavigationView() {
+
+        //Configuramos drawer toggle para manejar los estados de
+        // expandido y no expandido
+        mDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout,
+                R.string.app_name, R.string.app_name);
+
+        drawerLayout.addDrawerListener(mDrawerToggle);
+
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+        navigate(R.id.nav_home);
+    }
+
+    private void navigate(int itemId) {
+        switch (itemId) {
+            case R.id.nav_home:
+                changeFragment(new PrimerFragment());
+                break;
+            case R.id.nav_histortial:
+                changeFragment(new SegundoFragment());
+                break;
+        }
+
+    }
+
+    private void changeFragment(Fragment fragmentInstance) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.root, fragmentInstance)
+                .commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+        int itemId = item.getItemId();
+        item.setChecked(true);
+        navigate(itemId);
+
+        return true;
+
     }
 }
---
+++ navigationview/src/main/java/com/ken/navigationview/fragments/PrimerFragment.java
@@ -0,0 +1,30 @@
+package com.ken.navigationview.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ken.navigationview.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PrimerFragment extends Fragment {
+
+
+    public PrimerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_primer, container, false);
+    }
+
+}
